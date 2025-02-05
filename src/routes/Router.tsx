af89cffdf5f86dd94bf242d62fa3a903cdda1467
ssumanlife <ksm9805@naver.com>
2025-02-05 11:43:44 +0900

    clone test

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,37 +5,40 @@ import { PATH } from '@/constants/path'
 import { createBrowserRouter } from 'react-router-dom'
 import ProtectedRoutes from './ProtectedRoutes'
 
-const Router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: `/asset_allocation/:algorithm?`,
-        element: (
-          <ProtectedRoutes>
-            <Main />
-          </ProtectedRoutes>
-        )
-      },
-      {
-        path: PATH.invest.nav,
-        element: <Other />
-      },
-      {
-        path: PATH.factor_strategy.nav,
-        element: <Other />
-      },
-      {
-        path: PATH.strategy.nav,
-        element: <Other />
-      },
-      {
-        path: PATH.partnership.nav,
-        element: <Other />
-      }
-    ]
-  }
-])
+const Router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: `/asset_allocation/:algorithm?`,
+          element: (
+            <ProtectedRoutes>
+              <Main />
+            </ProtectedRoutes>
+          )
+        },
+        {
+          path: PATH.invest.nav,
+          element: <Other />
+        },
+        {
+          path: PATH.factor_strategy.nav,
+          element: <Other />
+        },
+        {
+          path: PATH.strategy.nav,
+          element: <Other />
+        },
+        {
+          path: PATH.partnership.nav,
+          element: <Other />
+        }
+      ]
+    }
+  ],
+  { basename: '/quantus' }
+)
 
 export default Router
